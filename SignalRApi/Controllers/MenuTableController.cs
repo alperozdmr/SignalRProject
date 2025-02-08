36008b519aa5cf9c48f06ae4de58028be410fedf
Alper Özdemir <alozdemir23@gmail.com>
2025-02-08 14:12:33 +0300

    SignalR İşlemlerinin çoğu yazıldı

--- SignalRApi/Controllers/MenuTableController.cs
+++ SignalRApi/Controllers/MenuTableController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MenuTableDto;
+using SignalR.EntityLayer;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApı.Controllers
 {
@@ -14,9 +17,52 @@ namespace SignalRApı.Controllers
 		{
 			_menuTableService = menuTableService;
 		}
-		[HttpGet]
+		[HttpGet("MenuTableCount")]
 		public IActionResult MenuTableCount() {
 			return Ok(_menuTableService.TMenuTableCount());
 		}
-	}
+
+        [HttpGet("MenuTableList")]
+        public IActionResult MenuTableList()
+        {
+            var values = _menuTableService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateMenuTable(CreateMenuTableDto var)
+        {
+           MenuTable menuTable = new MenuTable()
+            {
+               Name = var.Name,
+               Status = var.Status,
+            };
+            _menuTableService.TAdd(menuTable);
+            return Ok("Masa kısmı başarılı bir şekilde eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            _menuTableService.TDelete(value);
+            return Ok("Masa alanı silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto var)
+        {
+            MenuTable menuTable = new MenuTable
+            {
+                MenuTableID = var.MenuTableID,
+                Name = var.Name,
+                Status = var.Status,
+            };
+            _menuTableService.TUpdate(menuTable);
+            return Ok("Masa alanı güncellendi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            return Ok(value);
+        }
+    }
 }
--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -13,7 +13,7 @@ namespace SignalRApı.Hubs
 		private readonly IMoneyCaseService _moneyCaseService;
 		private readonly IMenuTableService _menuTableService;
 		private readonly IBookingService _bookingService;
-        private readonly INotificationService _notificationService;
+		private readonly INotificationService _notificationService;
 		public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService, INotificationService notificationService)
 		{
 			_categoryService = categoryService;
@@ -24,7 +24,7 @@ namespace SignalRApı.Hubs
 			_bookingService = bookingService;
 			_notificationService = notificationService;
 		}
-
+		public static int clientCount {get; set;} = 0;
 		public async Task SendStatistic()
 		{
 			var value = _categoryService.TCategoryCount();
@@ -121,27 +121,27 @@ namespace SignalRApı.Hubs
 			var notificationListByFalse = _notificationService.TGetAllNotificationsByFalse();
 			await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
 		}
-		//public async Task GetMenuTableStatus()
-		//{
-		//    var value = _menuTableService.TGetListAll();
-		//    await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
-		//}
-		//public async Task SendMessage(string user, string message)
-		//{
-		//    await Clients.All.SendAsync("ReceiveMessage", user, message);
-		//}
-		//public override async Task OnConnectedAsync()
-		//{
-		//    clientCount++;
-		//    await Clients.All.SendAsync("ReceiveClientCount", clientCount);
-		//    await base.OnConnectedAsync();
-		//}
-		//public override async Task OnDisconnectedAsync(Exception? exception)
-		//{
-		//    clientCount--;
-		//    await Clients.All.SendAsync("ReceiveClientCount", clientCount);
-		//    await base.OnDisconnectedAsync(exception);
-		//}
+		public async Task GetMenuTableStatus()
+		{
+			var value = _menuTableService.TGetListAll();
+			await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
+		}
+		public async Task SendMessage(string user, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+		public override async Task OnConnectedAsync()
+		{
+			clientCount++;
+			await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+			await base.OnConnectedAsync();
+		}
+		public override async Task OnDisconnectedAsync(Exception? exception)
+		{
+			clientCount--;
+			await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+			await base.OnDisconnectedAsync(exception);
+		}
 
 	}
 }
